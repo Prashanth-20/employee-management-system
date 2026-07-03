6ce547e60499bc471831dbb056e8650a3c3c72ce
prashanth-damera <prashanthdamera32@gmail.com>
2026-07-03 12:01:14 +0530

    EMS-001: Implement add employee workflow

---
+++ src/main/java/com/company/ems/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.company.ems.repository;
+
+import com.company.ems.model.Employee;
+import java.util.ArrayList;
+
+public class EmployeeRepository {
+    private ArrayList<Employee> employees = new ArrayList<>();
+
+    public void save(Employee employee) {
+        employees.add(employee);
+    }
+}
